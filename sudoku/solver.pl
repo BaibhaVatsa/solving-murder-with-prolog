6ef1532ce68d6742d0099d3051d8c522272f97f5
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-08-13 05:32:22 -0500

    re-implemented member and all_distinct

--- sudoku/solver.pl
+++ sudoku/solver.pl
@@ -4,8 +4,8 @@
 % each 3x3 should be unique
 
 print_sol([Head|Tail]) :-
-    print(Head),
-    print('\n'),
+    write(Head),
+    write(', '),
     print_sol(Tail).
 
 valid_range([], _, _).
@@ -15,27 +15,32 @@ valid_range([Head|Tail], Start, End) :-
     Head =< End,
     valid_range(Tail, Start, End).
 
+not_in(_, []).
+not_in(Val, [Head|Tail]) :-
+    Val \= Head,
+    not_in(Val, Tail).
+
 all_distinct([]).
 all_distinct([Head|Tail]) :-
-    member(Head, Tail),
-    all_distinct([Tail|Head]).
+    all_distinct(Tail),
+    not_in(Head, Tail).
 
 all_valid([]).
 all_valid([Head|Tail]) :-
     all_distinct(Head),
     all_valid(Tail).
 
-%carve(Solution, Type, N) :-
+carve(Solution, Return, N, Type) :-
 
 solve(Puzzle, Solution) :-
     length(Puzzle, L),
     N = floor(sqrt(L)),
     Solution = Puzzle,
-    valid_range(Solution, 1, N),
-    carve(Solution, Rows, N),
-    carve(Solution, Cols, N),
-    carve(Solution, Blocks, N),
+    carve(Solution, Rows, N, 'rows'),
+    carve(Solution, Cols, N. 'cols'),
+    carve(Solution, Blocks, N, 'blocks'),
     all_valid(Rows, N),
     all_valid(Cols, N),
     all_valid(Blocks, N),
+    valid_range(Solution, 1, N),
     print_sol(Solution).
